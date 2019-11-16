60dceafad98cbbc2451584000f4adafdaa4e3ed2
Imran Bohoran <t.bohoran@elsevier.com>
2019-11-16 02:10:04 +0000

    Removing autowiring in favour of programmatic wiring

--- src/main/java/com/tib/payments/model/actions/CreatePayment.java
+++ src/main/java/com/tib/payments/model/actions/CreatePayment.java
@@ -2,15 +2,11 @@ package com.tib.payments.model.actions;
 
 import com.tib.payments.model.domain.Payment;
 import com.tib.payments.persistence.PaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class CreatePayment {
 
     private final PaymentRepository paymentRepository;
 
-    @Autowired
     public CreatePayment(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
     }
--- src/main/java/com/tib/payments/model/actions/DeletePayment.java
+++ src/main/java/com/tib/payments/model/actions/DeletePayment.java
@@ -2,17 +2,13 @@ package com.tib.payments.model.actions;
 
 import com.tib.payments.model.domain.Payment;
 import com.tib.payments.persistence.PaymentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
 public class DeletePayment {
 
     private final PaymentRepository paymentRepository;
 
-    @Autowired
     public DeletePayment(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
     }
